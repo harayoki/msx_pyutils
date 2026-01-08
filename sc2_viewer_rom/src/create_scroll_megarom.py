100b6ac9d98d2348b4ddece40a5cf4db096b4d9b
harayoki <harayoki@users.noreply.github.com>
2026-01-09 01:25:22 +0900

    Use absolute jump for auto page check

--- sc2_viewer_rom/src/create_scroll_megarom.py
+++ sc2_viewer_rom/src/create_scroll_megarom.py
@@ -1604,7 +1604,7 @@ def build_boot_bank(
     JP_NZ(b, "CHECK_AUTO_PAGE")
     LD.A_mn16(b, ADDR.AUTO_SCROLL_TURN_STATE)
     CP.n8(b, 1)
-    JR_NZ(b, "CHECK_AUTO_PAGE")
+    JP_NZ(b, "CHECK_AUTO_PAGE")
     LD.A_n8(b, 2)
     LD.mn16_A(b, ADDR.AUTO_SCROLL_TURN_STATE)
     JP(b, "CHECK_AUTO_PAGE")
